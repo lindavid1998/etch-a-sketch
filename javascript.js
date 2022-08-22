3992e82b7e75e79ac47ea8e9bd3620b2f8f6bf09
David Lin <lindavid1998@gmail.com>
2022-08-21 19:45:56 -0700

    Create base grid

---
+++ javascript.js
@@ -0,0 +1,18 @@
+function createGrid() {
+    let container = document.querySelector('.container')
+    
+    for (let i = 0; i < 16; i++) {
+        let row = document.createElement('div')
+        row.className = 'row'
+        for (let j = 0; j < 16; j++) {
+            let div = document.createElement('div')
+            div.style.cssText = 'flex: none; width: 50px; height: 50px; border: 1px solid black; padding: 2px;'
+            row.appendChild(div)
+        }
+        row.style.cssText = 'display: flex; gap: 3px; justify-content: center;'
+        container.appendChild(row)
+    }
+    container.style.cssText = 'display: flex; flex-direction: column; gap: 5px;'
+}
+
+createGrid()
