a08c12186727567f7af0da088ae6b130b501cfbb
David Lin <lindavid1998@gmail.com>
2022-08-22 18:07:16 -0700

    Change etch color to be random RGB value

--- javascript.js
+++ javascript.js
@@ -17,7 +17,9 @@ function createGrid(dim = 16) {
 }
 
 function updateBackgroundColor(e) {
-    e.target.style.backgroundColor = 'black'
+    let randomColor = Math.floor(Math.random()*16777215).toString(16);
+    e.target.style.backgroundColor = '#' + randomColor;
+    // e.target.style.backgroundColor = 'black'
 }
 
 createGrid()
