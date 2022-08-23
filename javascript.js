d52ab6cce11eca45cd79904665c1109b0b1dbfe5
David Lin <lindavid1998@gmail.com>
2022-08-22 17:54:57 -0700

    Add functionality to set grid size button

--- javascript.js
+++ javascript.js
@@ -1,10 +1,10 @@
-function createGrid() {
+function createGrid(dim = 16) {
     let container = document.querySelector('.container')
     
-    for (let i = 0; i < 16; i++) {
+    for (let i = 0; i < dim; i++) {
         let row = document.createElement('div')
         row.className = 'row'
-        for (let j = 0; j < 16; j++) {
+        for (let j = 0; j < dim; j++) {
             let div = document.createElement('div')
             div.className = 'box'
             div.style.cssText = 'flex: none; width: 50px; height: 50px; border: 1px solid black; padding: 2px;'
@@ -16,14 +16,19 @@ function createGrid() {
     container.style.cssText = 'display: flex; flex-direction: column; gap: 5px;'
 }
 
+function updateBackgroundColor(e) {
+    e.target.style.backgroundColor = 'black'
+}
+
 createGrid()
 
 let boxes = document.querySelectorAll('.box')
 boxes.forEach(box => box.addEventListener('mouseover', updateBackgroundColor))
 
-function updateBackgroundColor(e) {
-    e.target.style.backgroundColor = 'black'
-}
+let clearButton = document.querySelector('.clear')
+clearButton.addEventListener('click', () => {
+    boxes.forEach(box => box.style.backgroundColor = '')
+})
 
 let button = document.querySelector('.set-size')
 button.addEventListener('click', () => {
@@ -31,10 +36,19 @@ button.addEventListener('click', () => {
     do {
         input = prompt('Enter a grid size (max: 100)')
     } while (input > 100);
-})
 
+    // remove all existing boxes
+    // boxes.forEach(box => box.remove());
+    let container = document.querySelector('.container')
+    while (container.firstChild) {
+        container.removeChild(container.firstChild)
+    }
 
-let clearButton = document.querySelector('.clear')
-clearButton.addEventListener('click', () => {
-    boxes.forEach(box => box.style.backgroundColor = '')
+    // create new grid using input
+    createGrid(input);
+
+    // add mouseover event listener to each box on new grid
+    boxes = document.querySelectorAll('.box')
+    boxes.forEach(box => box.addEventListener('mouseover', updateBackgroundColor))
+
 })
